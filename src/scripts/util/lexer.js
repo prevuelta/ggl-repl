cf65737534970025d4f810e07fc2d695a4280ac6
Pablo <hello@pablorevuelta.com>
2019-06-26 07:50:33 +1200

    auto-commit-1561492233

--- src/scripts/util/lexer.js
+++ src/scripts/util/lexer.js
@@ -13,16 +13,48 @@ const grammar = {
     },
 };
 
+const unit = 10;
+
+const mappings = {
+    '\\d+u$': str => {
+        const arr = str.split('u');
+        return +arr[0] * unit;
+    },
+};
+
 export default function(string) {
     const lines = string.split('\n');
     const tokens = lines.map(line => {
-        const [ref, ...args] = line.split(' ');
+        let args, ref;
+        if (/^\d/.test(string)) {
+            ref = 'p';
+            args = line.split(' ');
+        } else {
+            const arr = line.split(' ');
+            ref = arr.shift();
+            args = arr;
+        }
+        args = args.map(arg => {
+            let newArg = arg;
+            Object.keys(mappings).forEach(regStr => {
+                const reg = new RegExp(regStr);
+                if (reg.test(arg)) {
+                    const raw = reg.exec(arg)[0];
+                    console.log(raw);
+                    const fn = mappings[regStr];
+                    newArg = fn(arg);
+                }
+            });
+            return newArg;
+        });
         return {
             ref,
             args: args.join(' '),
             // args: args.map(arg => +arg),
         };
     });
+
+    console.log('Tokens', tokens);
     // lexemes
     // { ref: 'l', args: ['', '', 0] }
     // { name: 'line', args: [0, 0, 100, 100 ] }
