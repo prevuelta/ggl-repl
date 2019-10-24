ae98afb27bc94990e1bff7588f777281cfd513a3
p787567 <p787567@drlxxx00.nz.thenational.com>
2019-10-25 09:11:29 +1300

    Moved from rollup to webpacl

--- src/scripts/lib.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
-import AceEditor from 'react-ace';
-import 'brace/theme/github';
-export { React, ReactDOM, ReactDOMServer, AceEditor };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Workspace from './workspace';
-
 import Store from './data/store';
-
 import './util/keys';
+import 'brace/theme/github';
 
 const render = () => {
-    ReactDOM.render(
-        <Workspace state={Store.getState()} />,
-        document.getElementById('app')
-    );
+    ReactDOM.render(<Workspace state={Store.getState()} />, document.getElementById('app'));
 };
 
 render();
