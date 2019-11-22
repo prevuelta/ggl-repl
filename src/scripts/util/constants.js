d4c8975d0ce1174f641bd04b3a23c71f74dba046
Pablo Revuelta <pablo_revuelta@bnz.co.nz>
2019-11-22 15:29:48 +1300

    Moved from rollup to webpacl

--- src/scripts/util/constants.js
+++ src/scripts/util/constants.js
@@ -1,30 +1,32 @@
 export default {
-    SILVER_RATIO: Math.sqrt(2),
-    GOLDEN_RATIO: (1 + Math.sqrt(5)) / 2,
-    DEFAULT_ZOOM_LEVEL: 24,
-    CANVAS_SIZE: 280,
+  SILVER_RATIO: Math.sqrt(2),
+  GOLDEN_RATIO: (1 + Math.sqrt(5)) / 2,
+  DEFAULT_ZOOM_LEVEL: 24,
+  CANVAS_SIZE: 280,
 };
 
 export const COLORS = {
-    BLUE: '#27dded',
-    RED: '#e13d1c',
-    GREEN: '#14bb5b',
-    CREAM: '#ece5d4',
+  BLACK: '#000000',
+  WHITE: '#fff000',
+  BLUE: '#27dded',
+  RED: '#e13d1c',
+  GREEN: '#14bb5b',
+  CREAM: '#ece5d4',
 };
 
 export const modes = {
-    BROWSE: 'browse',
-    DOCUMENT: 'document',
-    EDIT: 'edit',
-    PREVIEW: 'preview',
+  BROWSE: 'browse',
+  DOCUMENT: 'document',
+  EDIT: 'edit',
+  PREVIEW: 'preview',
 };
 
 export const MODE_TAGS = {
-    [modes.DOCUMENT]: 'Document',
-    [modes.PREVIEW]: 'Preview',
+  [modes.DOCUMENT]: 'Document',
+  [modes.PREVIEW]: 'Preview',
 };
 
 export const POINT_TYPES = {
-    STRAIGHT: 0,
-    ARC: 1,
+  STRAIGHT: 0,
+  ARC: 1,
 };
