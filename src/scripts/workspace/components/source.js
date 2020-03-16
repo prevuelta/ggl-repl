85f012a4a85efc144e5ba8af8f36f5853858d464
Pablo Revuelta <pablo_revuelta@bnz.co.nz>
2020-03-16 16:20:35 +1300

    wip-16:20-16-03-20

--- src/scripts/workspace/components/source.js
+++ src/scripts/workspace/components/source.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import RuneEditor from './runeEditor';
+import React, { useState, useRef, useEffect } from "react";
+import AceEditor from "react-ace";
+import CustomRsMode from "./customRuneScriptMode";
 
-export default props => {
-    return (
-        <div className="source-inspector">
-            <RuneEditor
-                tabSize={2}
-                className="editable"
-                value={props.value}
-                onChange={(value, editor) => props.parseInput(value, editor)}
-                onCursorChange={(v, h) => props.handleCursorChange(v, h)}
-            />
-        </div>
-    );
+export default ({ value, parseInput, handleCursorChange }) => {
+  const aceEditor = useRef();
+  useEffect(() => {
+    const customRsMode = new CustomRsMode();
+    aceEditor.current.editor.getSession().setMode(customRsMode);
+  }, []);
+  return (
+    <div className="source-inspector">
+      <AceEditor
+        tabSize={2}
+        className="editable"
+        theme="github"
+        mode="text"
+        editorProps={{ $blockScrolling: true }}
+        ref={aceEditor}
+        value={value}
+        onChange={(value, editor) => parseInput(value, editor)}
+        onCursorChange={(v, h) => handleCursorChange(v, h)}
+      />
+      ;
+    </div>
+  );
 };
