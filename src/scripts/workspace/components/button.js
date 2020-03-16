0920cc94586451f981d3852aac223cd9bbc99304
Pablo Revuelta <pablo_revuelta@bnz.co.nz>
2020-03-17 08:00:00 +1300

    wip-08:00-17-03-20

--- src/scripts/workspace/components/button.js
+++ src/scripts/workspace/components/button.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function(props) {
-    const classes = `button ${props.icon ? 'icon' : ''}`;
-    return (
-        <button type="button" className={classes} onClick={props.onClick}>
-            {props.children || props.symbol}
-        </button>
-    );
+  const classes = `button ${props.icon ? "icon" : ""}`;
+  return (
+    <button
+      {...props}
+      type="button"
+      className={classes}
+      onClick={props.onClick}
+    >
+      {props.children || props.symbol}
+    </button>
+  );
 }
--- src/scripts/workspace/components/statusBar.js
+++ src/scripts/workspace/components/statusBar.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Button from './button';
-import { MODE_TAGS } from '../../util/constants';
-import Notifications from './notifications';
+import React from "react";
+import Button from "./button";
+import { MODE_TAGS } from "../../util/constants";
+import Notifications from "./notifications";
 
-export default ({ mode, save, edit, message, rune }) => (
-    <div className="statusbar">
-        <span className="tag">Rune</span>
-        {rune && (
-            <>
-                <Button onClick={save}>Save</Button>
-                <Button onClick={edit}>Edit</Button>
-            </>
-        )}
-        {rune && <p>{rune.name}</p>}
-        <Notifications notifications={[message]} />
-    </div>
+export default ({ mode, save, edit, message, rune, help }) => (
+  <div className="statusbar">
+    <span className="tag">Rune</span>
+    {rune && (
+      <>
+        <Button onClick={save}>Save</Button>
+        <Button onClick={edit}>Edit</Button>
+      </>
+    )}
+    {rune && <p>{rune.name}</p>}
+    <Notifications notifications={[message]} />
+    <Button style={{ "margin-left": "auto" }} onClick={help}>
+      Help
+    </Button>
+  </div>
 );
